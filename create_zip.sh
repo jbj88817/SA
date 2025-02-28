1e0e77016cccaf9d2efbf34a1dcd9507cbbbe35a
Bojie Jiang <jiangbojie@gmail.com>
2025-02-28 13:47:07 -0800

    Update zip scripts to use new project name

--- create_zip.sh
+++ create_zip.sh
@@ -3,13 +3,14 @@
 # Create a zip file of the project
 zip -r SA.zip \
     app/src/ \
-    app/build.gradle \
-    build.gradle \
+    app/build.gradle.kts \
+    build.gradle.kts \
+    settings.gradle.kts \
+    gradle/libs.versions.toml \
     gradle.properties \
     gradle/wrapper/ \
     gradlew \
     gradlew.bat \
-    settings.gradle \
     README.md
 
 echo "Project has been zipped to SA.zip" 
